4dea6c69247a554b12e11a105d41b061d702e0e3
Luis Perez <luisperezrivas.dev@gmail.com>
2022-09-17 13:50:10 -0700

    first commit

---
+++ easy/1-TwoSum/index.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      const a = nums[i];
+      const b = nums[j];
+
+      if (a + b === target) return [i, j];
+    }
+  }
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([3, 3], 6));
---
+++ easy/9-PalindromeNumber/index.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {};
+
+console.log(isPalindrome(121));
+console.log(isPalindrome(-121));
+console.log(isPalindrome(10));
